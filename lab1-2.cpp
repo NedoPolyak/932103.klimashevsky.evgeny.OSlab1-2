00e7b8083765e679afc366cf0ef926f9bd6a046b
NedoPolyak <61102390+NedoPolyak@users.noreply.github.com>
2023-12-16 15:33:29 +0700

    Update lab1-2.cpp

--- lab1-2.cpp
+++ lab1-2.cpp
@@ -11,39 +11,80 @@ pthread_cond_t cond_consumer = PTHREAD_COND_INITIALIZER;
 volatile int events[EVENTSNUM] = { 0 };
 
 void* producerThread(void* arg) {
-    for (int i = 0; i < EVENTSNUM; ++i) {
-        pthread_mutex_lock(&mutex);
-        events[i] = 1;
-        printf("Event %d Produced.\n", i + 1);
-        pthread_cond_signal(&cond_consumer);
-        pthread_cond_wait(&cond_producer, &mutex);
-        pthread_mutex_unlock(&mutex);
-    }
+for (int i = 0; i < EVENTSNUM; ++i) {
+sleep(1);
+if (pthread_mutex_lock(&mutex) != 0) {
+    fprintf(stderr, "Error locking mutex in producerThread\n");
     return nullptr;
+}
+events[i] = 1;
+printf("Event %d Produced.\n", i + 1);
+if (pthread_cond_signal(&cond_consumer) != 0) {
+fprintf(stderr, "Error signaling condition variable in producerThread\n");
+    pthread_mutex_unlock(&mutex);
+    return nullptr;
+}
+if (pthread_cond_wait(&cond_producer, &mutex) != 0) {
+fprintf(stderr, "Error waiting on condition variable in producerThread\n");
+    pthread_mutex_unlock(&mutex);
+    return nullptr;
+}
+if (pthread_mutex_unlock(&mutex) != 0) {
+    fprintf(stderr, "Error unlocking mutex in producerThread\n");
+    return nullptr;
+}
+}
+return nullptr;
 }
 
 void* consumerThread(void* arg) {
-    for (int i = 0; i < EVENTSNUM; ++i) {
-        pthread_mutex_lock(&mutex);
-        while (!events[i]) {
-            pthread_cond_wait(&cond_consumer, &mutex);
-        }
-        printf("Event %d Consumed.\n\n", i + 1);
-        events[i] = 0;
-        pthread_cond_signal(&cond_producer);
-        pthread_mutex_unlock(&mutex);
-    }
+for (int i = 0; i < EVENTSNUM; ++i) {
+if (pthread_mutex_lock(&mutex) != 0) {
+    fprintf(stderr, "Error locking mutex in consumerThread\n");
     return nullptr;
+}
+while (!events[i]) {
+if (pthread_cond_wait(&cond_consumer, &mutex) != 0) {
+    fprintf(stderr, "Error waiting on condition variable in consumerThread\n");
+    pthread_mutex_unlock(&mutex);
+return nullptr;
+}
+}
+printf("Event %d Consumed.\n\n", i + 1);
+events[i] = 0;
+if (pthread_cond_signal(&cond_producer) != 0) {
+    fprintf(stderr, "Error signaling condition variable in consumerThread\n");
+    pthread_mutex_unlock(&mutex);
+return nullptr;
+}
+if (pthread_mutex_unlock(&mutex) != 0) {
+    fprintf(stderr, "Error unlocking mutex in consumerThread\n");
+    return nullptr;
+}
+}
+return nullptr;
 }
 
 int main() {
-    pthread_t producer, consumer;
+pthread_t producer, consumer;
 
-    pthread_create(&producer, NULL, producerThread, NULL);
-    pthread_create(&consumer, NULL, consumerThread, NULL);
+if (pthread_create(&producer, NULL, producerThread, NULL) != 0) {
+    fprintf(stderr, "Error creating producer thread\n");
+    return 1;
+}
+if (pthread_create(&consumer, NULL, consumerThread, NULL) != 0) {
+    fprintf(stderr, "Error creating consumer thread\n");
+    return 1;
+}
 
-    pthread_join(consumer, NULL);
-    pthread_join(producer, NULL);
+if (pthread_join(consumer, NULL) != 0) {
+    fprintf(stderr, "Error joining consumer thread\n");
+    return 1;
+}
+if (pthread_join(producer, NULL) != 0) {
+    fprintf(stderr, "Error joining producer thread\n");
+    return 1;
+}
 
-    return 0;
+return 0;
 }
